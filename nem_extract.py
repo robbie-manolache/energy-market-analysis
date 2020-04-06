ac7004717571291d57cbd6a4d05ef08940a681bd
robbie29 <37903975+robbie29@users.noreply.github.com>
2020-04-06 21:15:47 +1000

    added data file download capability, including download tracking

--- nem_extract.py
+++ nem_extract.py
@@ -2,6 +2,7 @@ import os
 import requests
 from io import BytesIO
 from zipfile import ZipFile
+from datetime import datetime as dt
 import pandas as pd
 from nem_tracker import NEM_tracker
 
@@ -24,10 +25,13 @@ class NEM_extractor(NEM_tracker):
         NEM_tracker.__init__(self, data_dir, base_url)
         self.tracker_dict = __gen_tracker_dict__(self.resources)
         self.selected_resource = None
+        self.resource_dir = None
+        self.current_tracker_path = None
         self.current_tracker_df = None
         self.timestamp_min = None
         self.timestamp_max = None
         self.time_range = None
+        self.download_df = None
 
     def select_resource(self):
         """
@@ -48,8 +52,16 @@ class NEM_extractor(NEM_tracker):
         else:
             pass
         file_name = self.tracker_dict[resource]
+        self.resource_dir = os.path.join(self.data_dir, file_name[:-4])
+        if os.path.isdir(self.resource_dir):
+            pass
+        else:
+            os.makedirs(self.resource_dir)
         file_path = os.path.join(self.tracker_dir, file_name)
-        self.current_tracker_df = pd.read_csv(file_path)
+        self.current_tracker_path = file_path
+        self.current_tracker_df = pd.read_csv(file_path, parse_dates=[
+            'TIMESTAMP', 'UPLOAD_DATE', 'DOWNLOAD_DATE'
+        ])
         self.timestamp_min = self.current_tracker_df.TIMESTAMP.min()
         self.timestamp_max = self.current_tracker_df.TIMESTAMP.max()
         self.time_range = [self.timestamp_min, self.timestamp_max]
@@ -57,6 +69,38 @@ class NEM_extractor(NEM_tracker):
     def adjust_time_range(self, date_min=None, date_max=None):
         """
         """
-        for i, date in enumerate(date_min, date_max):
+        for i, date in enumerate([date_min, date_max]):
             if date is not None:
                 self.time_range[i] = pd.to_datetime(date)
+
+    def set_download_df(self):
+        """
+        """
+        df = self.current_tracker_df.copy()
+        df = df[(df['TIMESTAMP']>=self.time_range[0])&
+                (df['TIMESTAMP']<=self.time_range[1])&
+                (~df.DOWNLOADED)]
+        self.download_df = df
+
+    def _download(self, url):
+        """
+        """
+        r = requests.get(url)
+        zf = ZipFile(BytesIO(r.content))
+        zf.extractall(self.resource_dir)
+
+    def download_files(self):
+        """
+        """
+        for idx, row in self.download_df.iterrows():
+            if self.current_tracker_df.loc[idx, 'DOWNLOADED']:
+                pass
+            else:
+                self._download(row['URL'])
+                now = pd.to_datetime(dt.now(
+                       ).strftime('%Y-%m-%d-%H:%M:%S'))
+                self.current_tracker_df.loc[
+                    idx, ['DOWNLOADED', 'DOWNLOAD_DATE']
+                ] = (True, now)
+        self.current_tracker_df.to_csv(self.current_tracker_path, 
+                                       index=False)
